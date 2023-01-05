d3f67b8a91d0c37d5f0ba7c9895b9726b63fb6d9
Dmytro <kotykhin_d@ukr.net>
2023-01-05 15:54:35 +0200

    auth update

--- src/hooks/isAuth.ts
+++ src/hooks/isAuth.ts
@@ -1,8 +1,11 @@
+import { useDispatch } from "react-redux";
+
 import { useQuery } from "@apollo/client";
 import { GET_USER_BY_TOKEN } from "apollo/query/getUser";
-import { useDispatch } from "react-redux";
+
 import { setUser } from "store/userSlice";
 import { IUser } from "types/userTypes";
+import { getToken } from "services/getToken";
 
 interface IResponse {
     getUserByToken: IUser;
@@ -11,6 +14,7 @@ interface IResponse {
 export const useAuth = (): { isSuccess: boolean; isError: boolean } => {
     const dispatch = useDispatch();
     const { error, data } = useQuery<IResponse>(GET_USER_BY_TOKEN, {
+        skip: !getToken(),
         onCompleted: (res) => {
             dispatch(setUser(res.getUserByToken));
             console.log(res.getUserByToken.message);
