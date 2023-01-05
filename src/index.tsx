06a20c32efd7611783266a098e10afe1055c6a07
Dmytro <kotykhin_d@ukr.net>
2023-01-05 20:39:20 +0200

    update to react router v6.4

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,36 @@
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import { Provider } from "react-redux";
+
 import { ApolloProvider } from '@apollo/client';
+import client from 'apollo/client';
 
 import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import client from 'apollo/client';
 import store from "./store/store";
-import App from './App';
+import { router } from './App';
 
 import './index.scss';
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#00a1b6",
+        },
+    },
+});
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <ApolloProvider client={client}>
-      <CssBaseline />
-      <App />
-    </ApolloProvider>
-  </Provider>
+    <Provider store={store}>
+        <ApolloProvider client={client}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <RouterProvider router={router} />
+            </ThemeProvider>
+        </ApolloProvider>
+    </Provider>
 );
